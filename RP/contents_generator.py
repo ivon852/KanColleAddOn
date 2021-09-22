3ca007540bca84b22923f94fa4635e0853dc8281
ivon852 <qj985n2@protonmail.com>
2021-09-22 12:44:29 +0800

    Add basic files

---
+++ RP/contents_generator.py
@@ -0,0 +1,17 @@
+#建立的檔案會有多餘的逗號
+from os import walk
+import os
+from os.path import join
+
+mypath = "./"
+filename="contents.json"
+
+if not os.path.isfile(filename):
+ fp = open(filename, "w")
+ fp.write('{"content":[')
+ for root, dirs, files in walk(mypath):
+  for f in files:
+   fullpath = join(root, f)
+   fp.write( '{"path":"' + fullpath.replace("./","").replace('\\',"/") + '"},')
+ fp.write('],"version":1}')
+ fp.close()
---
+++ RP/texture_list_generator.py
@@ -0,0 +1,13 @@
+#建立的檔案會有多餘的逗號
+import glob
+import os
+
+filename="./textures/texture_list.json"
+if not os.path.isfile(filename):
+ fp = open(filename, "w")
+ fp.write('[');
+ for texture in glob.glob("./textures/**/*.png")+glob.glob("./textures/**/*.tga")+glob.glob("./subpacks/kanmusu_subpack/textures/**/*.tga")+glob.glob("./subpacks/kanmusu_subpack/textures/**/*.png"):
+  bn = os.path.splitext(os.path.basename(texture))[1]
+  fp.write('"'+texture.replace(bn,"").replace("./","").replace("\\","/").replace("subpacks/kanmusu_subpack/","")+'",')
+ fp.write(']')
+ fp.close()
